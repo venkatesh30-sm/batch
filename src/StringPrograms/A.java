63cf31830f211fefbba20a58c518937c1aeb6021
venkatesh30-sm <venkateshsmtester@gmail.com>
2020-02-20 10:13:09 +0530

    done

--- src/StringPrograms/A.java
+++ src/StringPrograms/A.java
@@ -1,8 +1,14 @@
 package StringPrograms;
 
+import java.util.Scanner;
+
 public class A extends B {
 	public void hai() {
 	System.out.println("bye");
+	Scanner s=new Scanner(System.in);
+	String n = s.next();
+	System.out.println(n);
+	
 
 	}
 	public void bye() {
